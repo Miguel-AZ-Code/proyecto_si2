26ec9fcb859efe8e10b9348813037b2513a2d7fb
Andres Chavez <andresintel773@gmail.com>
2022-12-12 21:22:45 -0400

    migration

---
+++ database/migrations/2022_12_12_191139_create_facturas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nit');
+            $table->timestamp('fecha');
+            $table->float('total',9,2);
+            $table->unsignedBigInteger('pago_id')
+            ->foreign('pago_id')->references('id')->on('metododepagos')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas');
+    }
+};
