a9455108a1bca42f9e26a49a2f167e0f812ba511
Yasir <yasirarce70@gmail.com>
2022-12-14 13:43:59 -0400

    Correciones en la API CLIENTE 2.0

--- routes/api.php
+++ routes/api.php
@@ -29,13 +29,13 @@ Route::post("/signup",[AuthController::class,"signup"]);
 // Route::apiResource('marcas', MarcaController::class);
 // Route::apiResource('medidas', MedidaController::class);
 // Route::apiResource('servicios', ServicioController::class);
+Route::post("/documentos",[ClienteController::class,"postDocumento"]);
 
 //TODO: RUTAS PROTEGIDAS POR SANCTUM
 Route::group(['middleware'=>'auth:sanctum'],function () {
     Route::post("/logout",[AuthController::class,"logout"]);
     Route::get("/contracto",[ClienteController::class,"getContratos"]);
     Route::get("/documento",[ClienteController::class,"getDocumento"]);
-    Route::post("/documentos",[ClienteController::class,"postDocumento"]);
 
 });
 
